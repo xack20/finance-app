be9b117b8221e4a65e3737cc5b30dfae076f82a5
Zakaria Hossain Foysal <zakariahossain20@gmail.com>
2026-05-28 08:30:09 +0600

    feat(android): add Application subclass

---
+++ composeApp/src/androidMain/kotlin/app/hisaab/HisaabApplication.kt
@@ -0,0 +1,10 @@
+package app.hisaab
+
+import android.app.Application
+
+class HisaabApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Phase P0b: initialize SQLDelight, Supabase client, crash reporter
+    }
+}
