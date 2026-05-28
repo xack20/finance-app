8f4c2814a27cf932781dd6e69e67c36cb14aea0a
Zakaria Hossain Foysal <zakariahossain20@gmail.com>
2026-05-28 08:28:27 +0600

    feat(app): add App() root composable

---
+++ composeApp/src/commonMain/kotlin/app/hisaab/App.kt
@@ -0,0 +1,12 @@
+package app.hisaab
+
+import androidx.compose.runtime.Composable
+import app.hisaab.design.HisaabTheme
+import app.hisaab.screens.today.TodayScreen
+
+@Composable
+fun App() {
+    HisaabTheme {
+        TodayScreen()
+    }
+}
