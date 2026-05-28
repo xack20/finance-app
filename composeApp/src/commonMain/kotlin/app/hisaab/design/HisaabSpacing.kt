997a4ef84f7a46ab4817c78e15e8d529297ea299
Zakaria Hossain Foysal <zakariahossain20@gmail.com>
2026-05-28 08:25:24 +0600

    feat(design): add HisaabSpacing 4dp base grid + 22dp gutter

---
+++ composeApp/src/commonMain/kotlin/app/hisaab/design/HisaabSpacing.kt
@@ -0,0 +1,13 @@
+package app.hisaab.design
+
+import androidx.compose.ui.unit.dp
+
+object HisaabSpacing {
+    val xs     = 4.dp
+    val sm     = 8.dp
+    val md     = 12.dp
+    val lg     = 16.dp
+    val gutter = 22.dp
+    val xl     = 32.dp
+    val xxl    = 48.dp
+}
---
+++ composeApp/src/commonTest/kotlin/app/hisaab/design/HisaabSpacingTest.kt
@@ -0,0 +1,18 @@
+package app.hisaab.design
+
+import androidx.compose.ui.unit.dp
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HisaabSpacingTest {
+    @Test
+    fun spacing_scale_uses_4dp_base_grid() {
+        assertEquals(4.dp,  HisaabSpacing.xs)
+        assertEquals(8.dp,  HisaabSpacing.sm)
+        assertEquals(12.dp, HisaabSpacing.md)
+        assertEquals(16.dp, HisaabSpacing.lg)
+        assertEquals(22.dp, HisaabSpacing.gutter)
+        assertEquals(32.dp, HisaabSpacing.xl)
+        assertEquals(48.dp, HisaabSpacing.xxl)
+    }
+}
