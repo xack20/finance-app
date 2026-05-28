4e01d1555bf20481301fbf30a1879311ed83efb7
Zakaria Hossain Foysal <zakariahossain20@gmail.com>
2026-05-28 08:32:12 +0600

    feat(ios): expose MainViewController for UIKit hosting

---
+++ composeApp/src/iosMain/kotlin/app/hisaab/MainViewController.kt
@@ -0,0 +1,8 @@
+package app.hisaab
+
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
+
+fun MainViewController(): UIViewController = ComposeUIViewController {
+    App()
+}
